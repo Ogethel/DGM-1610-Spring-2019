077a33f64b0fe3c5ff4266a175e85adacf97964c
McBlade <10718916@my.uvu.edu>
2019-02-25 11:35:04 -0700

    Class 2/25/19
    Worked on conditional statements (bools).  Homework is to turn a light on and off in unity. Also worked on character controller.

---
+++ Lizard_Platformer/Assets/Scripts/Conditionals.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conditionals : MonoBehaviour
+{
+    public bool CanWalk;
+    public int Number;
+    public string Password;
+
+    void Update()
+    {
+        print(CanWalk ? "True" : "false");
+
+        print(Number >= 10 ? "Greater" : "Lesser");
+
+        print(Password == "OU812" ? "The password is correct" : "The password is incorrect");
+    }
+}
