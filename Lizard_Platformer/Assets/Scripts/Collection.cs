51421a7ac5d5c7c80026430c9ff192c462a92218
McBlade <10718916@my.uvu.edu>
2019-03-25 11:09:49 -0600

    Class 3/25/19
    Added collections, Anthony helped fix Health Bar bug.

---
+++ Lizard_Platformer/Assets/Scripts/Collection.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class Collection : MonoBehaviour
+{
+   public List<Object> CollectionList;
+   
+   public void Collect(GameObject obj)
+   {
+      CollectionList.Add(obj);
+   }
+}
