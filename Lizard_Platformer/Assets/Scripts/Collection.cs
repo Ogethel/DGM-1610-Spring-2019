601caeb22453b39b394b665c6c5dae6f565c082a
McBlade <10718916@my.uvu.edu>
2019-03-27 11:22:38 -0600

    Class 3/27/19
    Working with Collections again.

--- Lizard_Platformer/Assets/Scripts/Collection.cs
+++ Lizard_Platformer/Assets/Scripts/Collection.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 [CreateAssetMenu]
-public class Collection : MonoBehaviour
+public class Collection : ScriptableObject
 {
    public List<Object> CollectionList;
    
    public void Collect(GameObject obj)
    {
       CollectionList.Add(obj);
+   }
+
+   public void CollectionInfo()
+   {
+      foreach (var obj in CollectionList)
+      {
+         Debug.Log(obj);
+      }
    }
 }
