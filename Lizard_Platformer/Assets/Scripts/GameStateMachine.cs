5bab73ea9d409932b9a8857a26115a4f13cdde6e
McBlade <10718916@my.uvu.edu>
2019-04-03 10:13:28 -0600

    Auto stash before merge of "master" and "origin/master"

---
+++ Lizard_Platformer/Assets/Scripts/GameStateMachine.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class GameStateMachine : ScriptableObject
+{
+    public UnityEvent StartingEvent, PlayingEvent, EndingEvent;
+
+    public enum States
+    {
+        Starting,
+        Playing,
+        Ending
+    }
+
+    public States CurrentStates;
+
+    public void ChangeState(States newState)
+    {
+        CurrentStates = newState;
+    }
+
+    public void OnSwitch()
+    {
+        switch (CurrentStates)
+        {
+            case States.Starting:
+                StartingEvent.Invoke();
+                break;
+            case States.Playing:
+                PlayingEvent.Invoke();
+                break;
+            case States.Ending:
+                EndingEvent.Invoke();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
---
+++ Lizard_Platformer/Assets/Scripts/Switches.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Switches : MonoBehaviour
+{
+    public UnityEvent StartingEvent, PlayingEvent, DyingEvent, EndingEvent;
+    
+    public enum States
+    {
+        Starting,
+        Playing,
+        Dying,
+        Ending
+    }
+
+    public States CurrentState;
+    
+ 
+    private void Update()
+    {
+        switch (CurrentState)
+        {
+            case States.Starting:
+                break;
+                StartingEvent.Invoke();
+            case States.Playing:
+                break;
+                PlayingEvent.Invoke();
+            case States.Ending:
+                break;
+                DyingEvent.Invoke();
+            case States.Dying:
+                break;
+                EndingEvent.Invoke();
+            default:
+                throw new ArgumentOutOfRangeException();
+        }  
+    }
+}
