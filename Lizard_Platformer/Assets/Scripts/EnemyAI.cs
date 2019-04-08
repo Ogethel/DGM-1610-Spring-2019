32a579c1d1550197f52ce1b672e77800fb160cff
McBlade <10718916@my.uvu.edu>
2019-04-08 11:10:45 -0600

    Class 4/8/19
    Worked on creating Enemy AI script, using nav-mesh. My Nav-mesh wouldn't create jumping points... Need to Look into that.

---
+++ Lizard_Platformer/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    public Transform player;
+    
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void OnTriggerStay(Collider other)
+    {
+        agent.destination = player.position;
+    }
+}
