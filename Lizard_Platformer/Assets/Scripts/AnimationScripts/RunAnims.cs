1904afddfadf865c62ed70bad9a69e3c0692b8e3
McBlade <10718916@my.uvu.edu>
2019-04-17 11:13:08 -0600

    Class 4/17/19
    Worked more on level design and also character controllers/animations.

---
+++ Lizard_Platformer/Assets/Scripts/AnimationScripts/RunAnims.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunAnims : MonoBehaviour
+{
+    private Animator anims;
+    public float Speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anims = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anims.SetFloat("Speed", Speed);
+    }
+}
