da1740c62aadde7054289747d4577dc0bbc49716
McBlade <10718916@my.uvu.edu>
2019-04-22 11:28:47 -0600

    Class 4/22/19
    Worked on Player Movement - Not working yet

---
+++ Lizard_Platformer/Assets/Scripts/Movement 3/Restart/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rgb;
+
+    private void start()
+    {
+        rgb.GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        PlayerMovement(rgb);
+    }
+
+    private void PlayerMovement(Rigidbody rigidBody)
+    {
+        if (Input.GetKey("a"))
+        {
+            rigidBody.velocity = new Vector2(-10, rigidBody.velocity.y);
+        }
+        
+        if (Input.GetKey("d"))
+        {
+            rigidBody.velocity = new Vector2(10, rigidBody.velocity.y);
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, 10);
+        }
+    }
+}
